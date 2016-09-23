b507498fc9457f4ddb7e704e016b44630324347c
luchanso <oleg@loutchansky.com>
2016-09-23 21:36:29 +0300

    Change screen size

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
-Engine.game = new Phaser.Game(600, 800, Phaser.AUTO);
+// 640x315
+// 335x640
+
+Engine.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO);
 
 Engine.game.state.add('Game', Engine.Game);
 Engine.game.state.start('Game');
