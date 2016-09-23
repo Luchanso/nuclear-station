75dbe618c7a8834fd6b98cdfa2a7e0d0347026e7
luchanso <oleg@loutchansky.com>
2016-09-23 21:37:00 +0300

    Finish clock timer

--- src/game/components/time.js
+++ src/game/components/time.js
@@ -1,11 +1,28 @@
 class Time extends Phaser.Text {
   constructor(game, x, y, time) {
     const style = {
-      font: '41px Open Sans',
+      font: '41px Orbitron',
       fill: 'rgb(236, 241, 0)'
     }
 
     super(game, x, y, time, style);
+  }
+
+  update() {
+    let date = new Date();
+    let ms = date.getMilliseconds().toString();
+
+    if (ms.length === 1) {
+      ms = '00' + ms;
+    } else if (ms.length === 2) {
+      ms = '0' + ms;
+    }
+
+    this.text = `${date.toLocaleTimeString()}.${ms}`;    
+  }
+
+  set time(value) {
+    this.text = time;
   }
 }
 
