ad346f83c5742265652c64fbb83e044ddf0b7c82
luchanso <oleg@loutchansky.com>
2016-09-23 21:36:47 +0300

    Fix some bugs in rect-button

--- src/game/components/rect-button.js
+++ src/game/components/rect-button.js
@@ -1,4 +1,9 @@
 class RectangleButton extends Phaser.Sprite {
+  set color(value) {
+    this.data.color = value;
+    this.tint = value;
+  }
+
   constructor(game, x, y, color, text) {
     const maxTextLength = 4;
 
@@ -12,17 +17,36 @@ class RectangleButton extends Phaser.Sprite {
     this.data.color = color;
 
     this.tint = this.data.color;
+    this.inputEnabled = true;
+
+    this.events.onInputDown.add(this.shakeMe, this);
 
     this.addText();
   }
 
+  shakeMe() {
+    this.game.add.tween(this)
+      .to({
+        x: this.x - 25
+      }, 20)
+      .to({
+        x: this.x + 50
+      }, 20)
+      .to({
+        x: this.x
+      }, 20)
+      .start();
+  }
+
   addText() {
+    const paddingTop = 3;
+
     const style = {
       font: '21px Open Sans',
       fill: 'white'
     }
 
-    this.lable = this.game.make.text(this.width / 2, this.height / 2, this.data.text, style);
+    this.lable = this.game.make.text(this.width / 2, this.height / 2 + paddingTop, this.data.text, style);
     this.lable.anchor.setTo(0.5);
     this.addChild(this.lable);
   }
